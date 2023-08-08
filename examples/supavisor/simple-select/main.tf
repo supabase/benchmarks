d95a3cd71b3c421b232a118172940bef6a1ca73e
egor <58992960+egor-romanov@users.noreply.github.com>
2023-08-08 11:50:01 +0300

    add supavisor bench (#25)

---
+++ examples/supavisor/simple-select/main.tf
@@ -0,0 +1,53 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.0.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+# Create an infrastructure with System Under Test (SUT).
+# module "setup_infra" {
+#   source = "./modules/setup"
+
+#   app_name = var.app_name
+#   fly_access_token = var.fly_access_token
+# }
+
+module "script" {
+  source = "./modules/script"
+
+  ami_id               = var.ami_id
+  instance_type        = var.instance_type
+  instances_count      = var.instances_count
+  security_group_id    = var.security_group_id
+  subnet_id            = var.subnet_id
+  sut_name             = var.sut_name
+  key_name             = var.key_name
+  private_key_location = var.private_key_location
+
+  testrun_name    = var.testrun_name
+  testrun_id      = var.testrun_id
+  test_origin     = var.test_origin
+  benchmark_id    = var.benchmark_id
+  supabench_token = var.supabench_token
+  supabench_uri   = var.supabench_uri
+
+  anon_token          = var.anon_token
+  service_token       = var.service_token
+  base_uri            = var.base_uri
+  conns               = var.conns
+  requests            = var.requests
+  rampscount          = var.rampscount
+  rampingduration     = var.rampingduration
+  consecutiveduration = var.consecutiveduration
+
+  # depends_on = [
+  #   module.setup_infra.ready,
+  # ]
+}
---
+++ examples/supavisor/simple-select/modules/setup/main.tf
@@ -0,0 +1,27 @@
+resource "aws_appautoscaling_target" "ecs_target" {
+  max_capacity       = 4
+  min_capacity       = 4
+  resource_id        = "service/a/a"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+
+  provisioner "local-exec" {
+    when    = destroy
+    command = "/aws --region ap-southeast-1 application-autoscaling register-scalable-target --service-namespace ecs --scalable-dimension ecs:service:DesiredCount --resource-id service/a/a --min-capacity 1 --max-capacity 1"
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+output "ready" {
+  # the value is not important because we're just
+  # using this for its dependencies.
+  value = {}
+
+  # Anything that refers to this output must wait until
+  # the actions for azurerm_monitor_diagnostic_setting.example
+  # to have completed first.
+  depends_on = [aws_appautoscaling_target.ecs_target]
+}
